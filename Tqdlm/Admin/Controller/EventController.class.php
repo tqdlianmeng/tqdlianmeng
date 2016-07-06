809414684470fdb1493afb9639a4e032ad7758a5
feisha5211 <wentaobeijing@126.com>
2016-07-07 01:17:54 +0800

    添加文件

--- Tqdlm/Admin/Controller/EventController.class.php
+++ Tqdlm/Admin/Controller/EventController.class.php
@@ -2,7 +2,18 @@
 namespace Admin\Controller;
 use Think\Controller;
 class EventController extends Controller {
-    public function index(){
+    public function index() {
         $this->display();
+    }
+
+    /**
+     * 添加赛事资讯
+     */
+    public function add() {
+    	if (IS_POST) {
+
+    	} else {
+    		$this->display();
+    	}
     }
 }
