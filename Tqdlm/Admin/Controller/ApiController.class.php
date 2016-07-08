88830d062b99bb1ce90e3e8f3debd1a4c496e941
feisha5211 <wentaobeijing@126.com>
2016-07-08 23:04:56 +0800

    完成新闻模块接口

--- Tqdlm/Admin/Controller/ApiController.class.php
+++ Tqdlm/Admin/Controller/ApiController.class.php
@@ -4,9 +4,12 @@ use Think\Controller;
 
 class ApiController extends ApiComController {
 
+	/**
+	 * 获取新闻列表
+	 */
 	public function news() {
 		$type = $_REQUEST['type'];
-		$page = empty($_REQUEST['p']) ? 1 : $_REQUEST['p'];
+		$p = empty($_REQUEST['p']) ? 1 : $_REQUEST['p'];
 		$types = array('0', '1', '2', '3');
 
 		if (empty($type) || !in_array($type, $types)) {
@@ -15,10 +18,108 @@ class ApiController extends ApiComController {
 			$this->output();
 		}
 
+		$page_size = 5;
 		$m_news = M('news');
-		$where = array('type' => $type);
+		$where = array('type' => $type, 'is_online' => '1');
+		$field = 'id, title, cover, type, content, crt_ts, view';
+
 		$count = $m_news->where($where)->count();
-		$page = new \Think\Page($count, 5);
-		
+		$total = intval(ceil($count / $page_size));
+
+		$page = new \Think\Page($count, $page_size);
+		$data = $m_news->where($where)->page($p.' , '.$page_size)->order('crt_ts DESC')->field($field)->select();
+		foreach($data as $k => &$v) {
+			if($v['type'] == '3') {
+				$v['cover'] = '';
+			}
+			$v['content'] = strip_tags($v['content']);
+			$v['crt_ts'] = date('Y-m-d H:i:s', $v['crt_ts']);
+		}
+
+		$result = array('item' => $data, 'current' => intval($p), 'total' => $total);
+		$this->setSucceeded(true);
+		$this->setResult($result);
+		$this->output();
+	}
+
+	/**
+	 * 获取新闻详情
+	 */
+	public function newsDetail() {
+		$id = $_REQUEST['id'];
+		if(empty($id)) {
+			$this->setErrCode(1);
+			$this->setErrMsg('id参数有误');
+			$this->output();
+		}
+
+		$field = 'id, title, cover, type, content, crt_ts, view, author';
+		$where = array('id' => $id, 'is_online' => '1');
+
+		$info = M('news')->where($where)->field($field)->find();
+		if (!empty($info['content']) && !empty($info['id'])) {
+			$info['content'] = htmlspecialchars($info['content']);
+			M('news')->where('id='.$id)->setInc('view');
+			$result = array('detail' => $info);
+			$this->setSucceeded(true);
+			$this->setResult($result);
+			$this->output();
+		} else {
+			$this->setErrCode(2);
+			$this->setErrMsg('内容不存在');
+			$this->output();
+		}
+	}
+
+	/**
+	 * 获取对应类型新闻的置顶新闻
+	 */
+	public function topNews() {
+		$type = $_REQUEST['type'];
+		$types = array('0', '1', '2', '3');
+
+		if (empty($type) || !in_array($type, $types)) {
+			$this->setErrCode(1);
+			$this->setErrMsg('类型参数有误');
+			$this->output();
+		}
+		$where = array('type' => $type, 'is_top' => '1', 'is_online' => '1');
+		$field = 'id, title, content, crt_ts, view, author';
+		$info = M('news')->where($where)->field($field)->order('crt_ts DESC')->limit('1')->find();
+		$info['crt_ts'] = date('Y-m-d H:i:s', $info['crt_ts']);
+		if (!empty($info['content'])) $info['content'] = strip_tags($info['content']);
+
+		$result = array('top' => $info);
+		$this->setSucceeded(true);
+		$this->setResult($result);
+		$this->output();
+	}
+
+	/**
+	 * 最新资讯
+	 */
+	public function latestNews() {
+		$m_news = M('news');
+		$field = 'title, type, id';
+		$info = $m_news->field($field)->limit('0 , 10')->select();
+		$result = array('item' => $info);
+
+		$this->setSucceeded(true);
+		$this->setResult($result);
+		$this->output();
+	}
+
+
+	public function getSlide() {
+		$type = $_REQUEST['type'];
+		$types = array('0', '1');
+		if (empty($type) || !in_array($type, $types)) {
+			$this->setErrCode(1);
+			$this->setErrMsg('type参数错误');
+			$this->output();
+		}
+
+		$m_news = M('slide');
+		$where = array('is_online' => '1');
 	}
 }
