a38919776d7b0a1b0db1531b8533f7666d28c337
feisha5211 <wentaobeijing@126.com>
2016-07-09 11:06:42 +0800

    修改上传路径

--- Tqdlm/Admin/Controller/NewsController.class.php
+++ Tqdlm/Admin/Controller/NewsController.class.php
@@ -189,7 +189,7 @@ class NewsController extends CommonController
         $upload->maxSize  = 3145728 ;// 设置附件上传大小
         $upload->exts     = array('jpg', 'png', 'jpeg', 'zip', 'rar');// 设置附件上传类型
         $upload->rootPath = 'Public/Uploads/'; // 设置附件上传根目录
-        $upload->savePath = 'news'; // 设置附件上传（子）目录
+        $upload->savePath = 'news/'; // 设置附件上传（子）目录
         // 上传文件
         $info = $upload->upload();
         if (!$info) {// 上传错误提示错误信息
